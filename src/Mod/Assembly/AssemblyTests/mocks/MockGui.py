de067208b75f7e37f12cbc90fdae423ace5f02e6
schmidtw <Weston_Schmidt@alumni.purdue.edu>
2025-10-11 08:35:18 -0700

    A more minimal approach to the changes.

--- src/Mod/Assembly/AssemblyTests/mocks/MockGui.py
+++ src/Mod/Assembly/AssemblyTests/mocks/MockGui.py
@@ -1,5 +1,5 @@
 # SPDX-License-Identifier: LGPL-2.1-or-later
-# /**************************************************************************
+# /****************************************************************************
 #                                                                           *
 #    Copyright (c) 2025 Weston Schmidt <weston_schmidt@alumni.purdue.edu>   *
 #                                                                           *
@@ -28,34 +28,28 @@ This module provides mock implementations of FreeCAD and Qt classes
 to enable unit testing without requiring the full FreeCAD environment.
 """
 
+# pylint: disable=too-few-public-methods
+
 import builtins
-from unittest.mock import MagicMock, patch
+from unittest.mock import MagicMock
 
 
 class MockQIcon:
-    """Mock QIcon class using MagicMock with spec."""
-    
-    def __init__(self, *args, **kwargs):
-        self._mock = MagicMock()
-        # Store the original arguments for potential use
-        self._args = args
-        self._kwargs = kwargs
-    
+    """Mock QIcon class."""
+
+    def __init__(self, *_args, **_kwargs):
+        pass
+
     @staticmethod
-    def fromTheme(theme_name, fallback=None):
+    def fromTheme(_theme_name, _fallback=None):
         """Mock fromTheme method."""
         return MockQIcon()
-    
-    def __getattr__(self, name):
-        """Delegate all other attributes to the MagicMock."""
-        return getattr(self._mock, name)
 
 
 class MockQTreeWidgetItem:
-    """Mock QTreeWidgetItem class using MagicMock with spec."""
+    """Mock QTreeWidgetItem class."""
 
-    def __init__(self, *args, **kwargs):
-        self._mock = MagicMock()
+    def __init__(self, *_args, **_kwargs):
         self.text_values = {}
         self.data_values = {}
         self._children = []
@@ -63,7 +57,6 @@ class MockQTreeWidgetItem:
     def setText(self, column, text):
         """Mock setText method."""
         self.text_values[column] = text
-        return self._mock.setText(column, text)
 
     def text(self, column):
         """Mock text method."""
@@ -72,11 +65,13 @@ class MockQTreeWidgetItem:
     def setData(self, column, role, data):
         """Mock setData method."""
         self.data_values[(column, role)] = data
-        return self._mock.setData(column, role, data)
 
     def data(self, column, role):
         """Mock data method."""
         return self.data_values.get((column, role))
+
+    def setIcon(self, _column, _icon):
+        """Mock setIcon method."""
 
     def childCount(self):
         """Mock method for getting child count."""
@@ -91,30 +86,19 @@ class MockQTreeWidgetItem:
     def addChild(self, child):
         """Mock method for adding a child item."""
         self._children.append(child)
-    
-    def __getattr__(self, name):
-        """Delegate all other attributes to the MagicMock."""
-        return getattr(self._mock, name)
 
 
 class MockSignal:
-    """Mock Signal class using MagicMock."""
+    """Mock Signal class."""
 
-    def __init__(self):
-        self._mock = MagicMock()
-
-
-
-    def __getattr__(self, name):
-        """Delegate all other attributes to the MagicMock."""
-        return getattr(self._mock, name)
+    def connect(self, _slot):
+        """Mock connect method."""
 
 
 class MockCheckBox:
-    """Mock CheckBox class using MagicMock."""
+    """Mock CheckBox class."""
 
     def __init__(self):
-        self._mock = MagicMock()
         self.checked = False
         self.stateChanged = MockSignal()
 
@@ -126,68 +110,44 @@ class MockCheckBox:
         """Mock isChecked method."""
         return self.checked
 
-    def __getattr__(self, name):
-        """Delegate all other attributes to the MagicMock."""
-        return getattr(self._mock, name)
 
 class MockButton:
-    """Mock Button class using MagicMock."""
+    """Mock Button class."""
 
     def __init__(self):
-        self._mock = MagicMock()
-        self._clicked_signal = MockSignal()
+        pass
 
     @property
     def clicked(self):
-        """Mock clicked signal."""
-        return self._clicked_signal
-
-    def __getattr__(self, name):
-        """Delegate all other attributes to the MagicMock."""
-        return getattr(self._mock, name)
+        """Mock clicked method."""
+        return MockSignal()
 
 
 class MockLineEdit:
-    """Mock LineEdit class using MagicMock."""
+    """Mock LineEdit class."""
 
     def __init__(self):
-        self._mock = MagicMock()
         self.textChanged = MockSignal()
-        self._text = ""
 
     def text(self):
         """Mock text method."""
-        return self._text
+        return ""
 
-    def setText(self, text):
+    def setText(self, _text):
         """Mock setText method."""
-        self._text = text
-
-    def __getattr__(self, name):
-        """Delegate all other attributes to the MagicMock."""
-        return getattr(self._mock, name)
 
 
 class MockHeader:
-    """Mock Header class using MagicMock."""
-
-    def __init__(self):
-        self._mock = MagicMock()
+    """Mock Header class."""
 
     def hide(self):
         """Mock hide method."""
-        pass
-
-    def __getattr__(self, name):
-        """Delegate all other attributes to the MagicMock."""
-        return getattr(self._mock, name)
 
 
 class MockPartList:
-    """Mock PartList class using MagicMock."""
+    """Mock PartList class."""
 
     def __init__(self):
-        self._mock = MagicMock()
         self.itemClicked = MockSignal()
         self.itemDoubleClicked = MockSignal()
         self._items = []
@@ -204,9 +164,11 @@ class MockPartList:
         """Mock addTopLevelItem method."""
         self._items.append(item)
 
+    def installEventFilter(self, _filter_obj):
+        """Mock installEventFilter method."""
+
     def expandAll(self):
         """Mock expandAll method."""
-        pass
 
     def topLevelItemCount(self):
         """Mock topLevelItemCount method."""
@@ -218,144 +180,95 @@ class MockPartList:
             return self._items[index]
         return None
 
-    def sizeHintForRow(self, row):
+    def sizeHintForRow(self, _row):
         """Mock sizeHintForRow method."""
         return 20
 
-    def setMinimumHeight(self, height):
+    def setMinimumHeight(self, _height):
         """Mock setMinimumHeight method."""
-        pass
-
-    def __getattr__(self, name):
-        """Delegate all other attributes to the MagicMock."""
-        return getattr(self._mock, name)
 
 
 class MockForm:
-    """Mock Form class using MagicMock."""
+    """Mock Form class."""
 
     def __init__(self):
-        self._mock = MagicMock()
         self.partList = MockPartList()
         self.CheckBox_ShowOnlyParts = MockCheckBox()
         self.CheckBox_RigidSubAsm = MockCheckBox()
         self.openFileButton = MockButton()
         self.filterPartList = MockLineEdit()
 
-    def installEventFilter(self, filter_obj):
+    def installEventFilter(self, _filter_obj):
         """Mock installEventFilter method."""
-        pass
 
-    def setWindowTitle(self, title):
+    def setWindowTitle(self, _title):
         """Mock setWindowTitle method."""
-        pass
 
     def show(self):
         """Mock show method."""
-        pass
 
     def hide(self):
         """Mock hide method."""
-        pass
-
-    def __getattr__(self, name):
-        """Delegate all other attributes to the MagicMock."""
-        return getattr(self._mock, name)
 
 
 class MockPySideUic:
-    """Mock PySideUic class using MagicMock."""
-
-    def __init__(self):
-        self._mock = MagicMock()
+    """Mock PySideUic class."""
 
     @staticmethod
-    def loadUi(ui_file):
+    def loadUi(_ui_file):
         """Mock loadUi method."""
         return MockForm()
 
-    def __getattr__(self, name):
-        """Delegate all other attributes to the MagicMock."""
-        return getattr(self._mock, name)
 
 def MockGetDocument(doc_name):
-    """Mock getDocument function using MagicMock."""
-    mock_doc = MagicMock()
-    mock_doc.Name = doc_name
-    mock_doc.getObject = MagicMock(return_value=None)
-    mock_doc.TreeRootObjects = []
-    return mock_doc
+    """Mock getDocument function."""
+    return type(
+        "MockGuiDocument",
+        (),
+        {
+            "Name": doc_name,
+            "getObject": lambda _obj_name: None,
+            "TreeRootObjects": [],
+        },
+    )()
 
 
-def MockAddModule(module_name):
-    """Mock addModule function using MagicMock."""
-    pass
+def MockAddModule(_module_name):
+    """Mock addModule function."""
 
 
-def MockDoCommandSkip(commands):
-    """Mock doCommandSkip function using MagicMock."""
-    pass
+def MockDoCommandSkip(_commands):
+    """Mock doCommandSkip function."""
 
 
 def SetupGuiMocks():
-    """Set up all FreeCAD GUI mocks for testing using unittest.mock."""
-    import sys
-    from types import ModuleType
-    import FreeCAD as App  # pylint: disable=import-error,import-outside-toplevel
+    """Set up all FreeCAD GUI mocks for testing."""
+    import FreeCADGui as Gui  # pylint: disable=import-error,import-outside-toplevel
+    from PySide import QtCore, QtGui  # pylint: disable=import-error,import-outside-toplevel
 
-    # Force GuiUp to be True so GUI-dependent code gets loaded
-    App.GuiUp = True
-
-    # Try to import FreeCADGui, create mock if it doesn't exist (CLI mode)
-    try:
-        import FreeCADGui as Gui  # pylint: disable=import-error,import-outside-toplevel
-    except ImportError:
-        # FreeCADGui doesn't exist (CLI mode), create a mock module
-        Gui = ModuleType('FreeCADGui')
-        sys.modules['FreeCADGui'] = Gui
-
-    # Try to import PySide modules, create mocks if they don't exist
-    try:
-        from PySide import QtCore, QtGui, QtWidgets  # pylint: disable=import-error,import-outside-toplevel
-    except ImportError:
-        # PySide doesn't exist, create mock modules
-        QtCore = ModuleType('PySide.QtCore')
-        QtGui = ModuleType('PySide.QtGui')
-        QtWidgets = ModuleType('PySide.QtWidgets')
-        # Create PySide parent module if needed
-        if 'PySide' not in sys.modules:
-            PySide = ModuleType('PySide')
-            PySide.QtCore = QtCore
-            PySide.QtGui = QtGui
-            PySide.QtWidgets = QtWidgets
-            sys.modules['PySide'] = PySide
-        sys.modules['PySide.QtCore'] = QtCore
-        sys.modules['PySide.QtGui'] = QtGui
-        sys.modules['PySide.QtWidgets'] = QtWidgets
-
-    # Add QObject to QtCore for TaskAssemblyInsertLink inheritance
-    if not hasattr(QtCore, 'QObject'):
-        QtCore.QObject = type('QObject', (), {})
-
-    # Replace QtGui classes directly (permanent replacement)
+    # Patch QtGui with our mock classes
     QtGui.QIcon = MockQIcon
     QtGui.QTreeWidgetItem = MockQTreeWidgetItem
 
     # Mock the PySideUic if it doesn't exist
     if not hasattr(Gui, "PySideUic"):
-        Gui.PySideUic = MockPySideUic()
+        Gui.PySideUic = MockPySideUic
 
     # Mock additional Gui methods that might be missing
     if not hasattr(Gui, "getDocument"):
         Gui.getDocument = MockGetDocument
 
-    # Mock Selection module using MagicMock
+    # Mock Selection module
     if not hasattr(Gui, "Selection"):
-        mock_selection = MagicMock()
-        mock_selection.clearSelection = MagicMock()
-        mock_selection.addSelection = MagicMock()
-        mock_selection.getSelection = MagicMock(return_value=[])
-        Gui.Selection = mock_selection
+        Gui.Selection = type(
+            "MockSelection",
+            (),
+            {
+                "clearSelection": lambda *args: None,
+                "addSelection": lambda *args: None,
+                "getSelection": lambda *args: [],
+            },
+        )()
 
     # Mock addModule method
     if not hasattr(Gui, "addModule"):
@@ -365,24 +278,9 @@ def SetupGuiMocks():
     if not hasattr(Gui, "doCommandSkip"):
         Gui.doCommandSkip = MockDoCommandSkip
 
-    # Mock addCommand method (used to register FreeCAD commands)
-    if not hasattr(Gui, "addCommand"):
-        Gui.addCommand = MagicMock()
-
-    # Mock Control.showDialog
-    if not hasattr(Gui, "Control"):
-        mock_control = MagicMock()
-        mock_control.showDialog = MagicMock()
-        Gui.Control = mock_control
-
-    # Mock activeDocument
-    if not hasattr(Gui, "activeDocument"):
-        Gui.activeDocument = MagicMock()
-
-    # Make QtCore, QtGui, QtWidgets and Gui available in the global namespace
+    # Make QtCore, QtGui and Gui available in the global namespace
     builtins.QtCore = QtCore
     builtins.QtGui = QtGui
-    builtins.QtWidgets = QtWidgets
     builtins.Gui = Gui
     builtins.QIcon = MockQIcon
 
